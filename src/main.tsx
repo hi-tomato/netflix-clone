72779d35d28c0a596742148360a1c88bc9b64882
juni <tmdwns0927@naver.com>
2025-04-16 18:34:31 +0900

    chore: React Router 경로 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import {
+  createBrowserRouter,
+  RouteObject,
+  RouterProvider
+} from 'react-router-dom';
+import NotFound from './pages/NotFound.tsx';
+import Videos from './pages/Videos.tsx';
+import VideoDetail from './pages/VideoDetail.tsx';
+
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Videos /> },
+      { path: '/videos', element: <Videos /> },
+      { path: '/videos/:keyword', element: <Videos /> },
+      { path: '/videos/watch/:watchId', element: <VideoDetail /> }
+    ]
+  }
+];
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
