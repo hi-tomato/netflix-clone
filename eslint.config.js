f48b0f1cc7ead4b25af639de5f085c197ca57c4c
juni <tmdwns0927@naver.com>
2025-04-16 18:53:55 +0900

    chore: Eslint-plugin-jsx-a11y (추가)

--- eslint.config.js
+++ eslint.config.js
@@ -8,11 +8,32 @@ export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     plugins: { js },
-    extends: ['js/recommended'],
+    extends: ['plugin:jsx-a11y/recommended'],
     rules: {
       'no-unused-vars': 'off',
       'no-undef': 'warn',
-      'no-constant-condition': 'warn'
+      'no-constant-condition': 'warn',
+
+      // 추가된 jsx-a11y 규칙
+      'jsx-a11y/anchor-is-valid': [
+        'warn',
+        {
+          components: ['Link'],
+          specialLink: ['to'],
+          aspects: ['noHref', 'invalidHref', 'preferButton']
+        }
+      ],
+      'jsx-a11y/alt-text': 'error', // 모든 이미지에 alt 속성 필수
+      'jsx-a11y/no-autofocus': 'warn', // autofocus 사용 경고
+      'jsx-a11y/label-has-associated-control': [
+        'error',
+        {
+          labelComponents: [],
+          labelAttributes: ['label'],
+          controlComponents: ['Input'],
+          depth: 3
+        }
+      ]
     }
   },
   {
